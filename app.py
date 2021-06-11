3e3172d8c761a81760c9efeaad92dce91352f0e8
lukexyz <luke.woods@OVO3735MB.lan>
2021-06-11 11:09:07 +0100

    connect to firebase

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
+from google.cloud import firestore
 
-st.header('Digg 9000 🌎!')
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firebase/clonewars-cd499.json")
+
+# Create a reference to the a post.
+doc_ref = db.collection("posts").document("Hacker News")
+
+st.header('Digg 9000 🌎')
+
 if st.button('Frontpage?'):
-    st.balloons()
+
+    # Get data at reference.
+    doc = doc_ref.get()
+
+    # Let's see what we got!
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
+
